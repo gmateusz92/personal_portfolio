5a2f38a5082c2236dcf20762d0f81ecf9e09b3cc
mateusz <matek1600@o2.pl>
2022-09-29 14:47:12 +0200

    1

--- blog/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Blog(models.Model):
-    title = models.CharField(max_length=100)
-    date = models.DateField(max_length=100)
-    descr = models.CharField(max_length=100)
